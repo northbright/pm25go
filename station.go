fe526327b84659b02f9b8f72c0a882dd274e49ed
Frank Xu <frank.xu@northbright.com>
2016-10-24 15:19:06 +0800

    Add comments for exported types.

--- station.go
+++ station.go
@@ -1,17 +1,20 @@
 package pm25
 
+// Position contains GEO position information.
 type Position struct {
-    Latitude float64
-    Longitude float64
+	Latitude  float64
+	Longitude float64
 }
 
+// StationInfo contains information of a station.
 type StationInfo struct {
-    Name string
-    City string
-    Location Position
+	Name     string
+	City     string
+	Location Position
 }
 
+// Station represents one station.
 type Station interface {
-    save(time string, pm25_hourly_data string, aqi string) (err error)
-    GrabData() (err error)
+	save(time string, pm25_hourly_data string, aqi string) (err error)
+	GrabData() (err error)
 }
